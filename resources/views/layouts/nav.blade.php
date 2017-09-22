1dd62292476ff253e873c268cf783b835cfeef92
ElangT <sayaelang@gmail.com>
2017-09-22 19:48:03 +0700

    Navbar Menu

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -6,14 +6,21 @@
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar">Transaksi</span>
+            <span class="icon-bar">Dashboard</span>
           </button>
 			    <a class="navbar-brand" href="{{url('/')}}">Kursusin</a>
         </div>
 
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
-            <li ><a href="#">Home</a></li>
-      			<li><a href="{{url('/checkout')}}">Transaksi</a></li>
+
+            @if (Auth::guest() && !Auth::guard('provider')->check() && strpos(Route::currentRouteName(), 'provider') !== false)
+            @elseif(Auth::guest() && !Auth::guard('provider')->check())
+            @elseif(Auth::guard('provider')->check())
+            <li><a href="{{url('/provider/dashboard')}}">Dashboard</a></li>
+            @else
+            <li><a href="{{url('/checkout')}}">Transaksi</a></li>
+            @endif
           </ul>
 
           <ul class="nav navbar-nav navbar-right">

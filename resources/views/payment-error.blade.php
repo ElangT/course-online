42745cd2135ca5c1646d391cbf013cdd786c2c97
nurintaaa <intannnalatas@gmail.com>
2017-10-25 21:34:08 +0700

    fix cache

--- resources/views/payment-error.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.master')
-
-@section('content')
-<h1>Payment Error</h1>
-<pre>{{$request}}</pre>
-@endsection
--- resources/views/payment.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.master')
-
-@section('content')
-<h1>Payment Notification</h1>
-@endsection

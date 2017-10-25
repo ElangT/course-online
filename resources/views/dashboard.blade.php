b1f392bedc265aae2792aadd3e8d2d20384bf511
nurintaaa <intannnalatas@gmail.com>
2017-10-25 23:59:43 +0700

    beautify dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.master')
 @section('additional-css')
+    <link href="{{ asset('/css/dashboard.css')}}" rel="stylesheet" type="text/css">
 @endsection
 @section('content')
-    <div class="row profile">
-        <div class="col-md-3">
+    <div class="row">
+        <div class="col-md-3" id="sidebar">
             <div class="panel">
             <div class="profile-sidebar panel">
                 <!-- SIDEBAR USERPIC -->
@@ -33,13 +34,26 @@
                     <ul class="nav">
                         <li class="active">
                             <a href="{{route('provider.edit')}}">
-                            <i class="glyphicon glyphicon-home"></i>
+                            <i class="glyphicon glyphicon-user"></i>
                             Change Profile </a>
                         </li>
                         <li>
                             <a href="{{route('provider.passwordedit')}}">
-                            <i class="glyphicon glyphicon-user"></i>
+                            <i class="glyphicon glyphicon-cog"></i>
                             Change Password </a>
+                        </li>
+                        <li id="nav-title">
+                            Manage Class/Course
+                        </li>
+                        <li>
+                            <a href="{{route('course.create')}}">
+                            <i class="glyphicon glyphicon-pencil"></i>
+                            Create Course</a>
+                        </li>
+                        <li>
+                            <a href="#">
+                            <i class="glyphicon glyphicon-barcode"></i>
+                            Manage Transaction</a>
                         </li>
                     </ul>
                 </div>
@@ -47,33 +61,47 @@
             </div>
         </div>
         <div class="col-md-9">
-            <div class="profile-content">
-                <h1>Manage Class/Course</h1>
-                <a href="{{route('course.create')}}">Create Course</a>
-                <a href="#">Manage Transaction</a>
-
-                @if (isset($courses))
+            @if (isset($courses))
                 <?php $result = 0; ?>
                 <div class="space">
                 @foreach ($courses as $course)
-                    <div class="panel panel-danger sharp-box space-item course">
-                        <div class="panel-body row">
-                            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                                <img class="img-thumbnail img-responsive" src="{{ asset('images/'.$image) }}">
-                            </div>
-                            <div class="parent col-lg-8 col-md-8 col-sm-8 col-xs-8">
-                                <h1 class=""><a href="{{ URL::to('/courses/' . $course->ak_course_id) }}">{{ $course->ak_course_name }}</a></h1>
-                                <h2 class="margin-left-sml child set-up set-right red">Rp {{ $course->ak_course_detail_price }}</h2>
-                                <h2 class="float-normal">{{ $course->ak_subcat_name }}   <span class="label label-success">{{ $course->ak_course_level_name }}</span>  <span class="label label-primary">{{ $course->ak_course_age_name_id }}</span></h2>
-                                <p class="margin-down-sml">{{ $course->ak_course_detail_desc }}</p>
-                                @foreach($course->schedule as $schedule)
-                                <p class="margin-down-sml">{{ $schedule->ak_course_schedule_day }}, {{ $schedule->ak_course_schedule_time }}</p>
-                                @endforeach
-                                <a href="{{ Route('course.update', $course->ak_course_id)}}">Edit</a>
-                                <a href="{{ URL::to('/provider/manage/' . $course->ak_course_id) }}">Manage</a>
-
-                            </div>
+                    <div class="panel panel-default sharp-box space-item course">
+                      <div class="panel-body">
+                        <div class="row">
+                          <div class="col-md-9 col-xs-6">
+                            <h1 class="course-title"><a href="{{ URL::to('/courses/' . $course->ak_course_id) }}">{{ $course->ak_course_name }}</a></h1>
+                          </div>
+                          <div class="col-md-3 col-xs-6 panel-setting">
+                            <a href="{{ Route('course.update', $course->ak_course_id)}}"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Edit</a>
+                            <a href="{{ URL::to('/provider/manage/' . $course->ak_course_id) }}"><span class="glyphicon glyphicon-wrench" aria-hidden="true"></span> Manage</a>
+                          </div>
                         </div>
+                        <div class="row">
+                          <div class="col-md-12 panel-description">
+                            <p>
+                              <span class="glyphicon glyphicon-calendar" aria-hidden="true"></span>
+                              <strong>Jadwal :</strong>
+                            </p>
+                            @foreach($course->schedule as $schedule)
+                            <p>{{ $schedule->ak_course_schedule_day }}, {{ $schedule->ak_course_schedule_time }}</p>
+                            @endforeach
+                            <p>{{ $course->ak_course_detail_desc }}</p>
+                          </div>
+                        </div>
+                        <div class="row">
+                          <div class="col-md-9">
+                            <h2 class="float-normal">
+                              <span class="glyphicon glyphicon-tags" aria-hidden="true"></span>
+                              <span class="label label-info">{{ $course->ak_subcat_name }}</span>
+                              <span class="label label-success">{{ $course->ak_course_level_name }}</span>
+                              <span class="label label-primary">{{ $course->ak_course_age_name_id }}</span>
+                            </h2>
+                          </div>
+                          <div class="col-md-3">
+                            <h2 class="price">Rp {{ $course->ak_course_detail_price }}</h2>
+                          </div>
+                        </div>
+                      </div>
                     </div>
                     <?php $result = $result + 1; ?>
                 @endforeach
